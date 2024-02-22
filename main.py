5712e945b30543eafefe5f42904bb1c5c39f4c60
Soheab_ <33902984+Soheab@users.noreply.github.com>
2024-02-22 17:38:14 +0100

    fix: add back command_prefix for jishaku

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
+from discord.ext import commands
 
 from utils.bot import WiresBot
 
-bot = WiresBot(command_prefix=(), intents=discord.Intents(guilds=True, emojis=True), strip_after_prefix=True)
+bot = WiresBot(command_prefix=commands.when_mentioned_or("wires"), intents=discord.Intents(guilds=True, emojis=True), strip_after_prefix=True)
 bot.run()
