3e3ac62c922ab65ec4235f15050ceb6cb95161c5
itslychee <itslychee@protonmail.com>
2024-02-23 13:54:21 +0000

    A

--- main.py
+++
@@ -1,7 +0,0 @@
-import discord
-from discord.ext import commands
-
-from utils.bot import WiresBot
-
-bot = WiresBot(command_prefix=commands.when_mentioned_or("wires"), intents=discord.Intents(guilds=True, emojis=True), strip_after_prefix=True)
-bot.run()
